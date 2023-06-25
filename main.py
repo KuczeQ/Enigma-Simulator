aa2ffee48a4697ee52e03dcc887fc7635de078e4
KuczeQ <65510168+KuczeQ@users.noreply.github.com>
2023-06-25 13:36:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from enigma_simulator import EnigmaSimulator
 
-config_file = 'enigma_config.py'  # Nazwa pliku konfiguracyjnego
+config_file = 'enigma_config.py' 
 
-# Utworzenie instancji symulatora Enigmy
 enigma = EnigmaSimulator(config_file)
 
-# Pytanie użytkownika o wiadomość do zaszyfrowania
 message = input("Podaj wiadomość do zaszyfrowania: ")
 
-# Szyfrowanie wiadomości
 encrypted_message = enigma.encrypt(message)
 print('Encrypted message:', encrypted_message)
